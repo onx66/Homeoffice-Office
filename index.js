313866944dc5c3a1d88a42419e2678f816e52f23
onx66 <oipek01@mail.com>
2024-07-16 11:08:32 +0300

    My first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const correctAnswers = ['Y', 'Y', 'Y', 'Y'];
+const form = document.querySelector(".question-form");
+const result = document.querySelector(".result");
+
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value,]
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }
+
+    })
+
+    // console.log(score);
+    result.classList.remove("d-none");
+    let puan = 0;
+    const bastır = setInterval(() => {
+        result.querySelector("span").textContent = `%${puan}`;
+        if(puan==score){
+            clearInterval(bastır);
+        }
+        else {
+            puan++;
+        }
+    }, 40);
+   
+    
+})
+
